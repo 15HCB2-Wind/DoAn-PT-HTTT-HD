7c05f9b05cb3691f622c5f48a2d9cbd7dbc85f08
phanhaugiang <thienthientan.tin@gmail.com>
2017-04-23 19:14:57 +0700

    Done DataAccess
    ChuongTrai KhoSua NhaCungCap PhanCong

---
+++ Wind_DrinkSmile_MiddleWare/DomainData/NhaCungCap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DomainData
+{
+    public class NhaCungCap
+    {
+        public string MaNCC { get; set; }
+        public string TenNCC { get; set; }
+        public string TinhTrang { get; set; }
+        public string DiaChi { get; set; }
+    }
+}
