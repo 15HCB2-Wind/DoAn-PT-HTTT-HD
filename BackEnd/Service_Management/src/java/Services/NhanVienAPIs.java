200baa2af90ec051f17f70465160a9858bfeac75
Tú Phạm <chungtu3108@yahoo.com.vn>
2017-05-23 20:24:57 +0700

    update no load password for staff

--- BackEnd/Service_Management/src/java/Services/NhanVienAPIs.java
+++ BackEnd/Service_Management/src/java/Services/NhanVienAPIs.java
@@ -52,6 +52,7 @@ public class NhanVienAPIs {
                     response.Errors.add("Không tìm thấy nhân viên.");
                     response.IsError = true;
                 } else {
+                    result.setMatkhau(null);
                     response.Data = result;
                 }
             } catch (Exception ex) {
@@ -73,6 +74,9 @@ public class NhanVienAPIs {
         if (BusinessHandler.TokenBUS.tokenCheck(request, response, 2)) {
             try {
                 List<Nhanvien> result = NhanVienAdapter.getAll(request.MaCN, request.MaPQ);
+                for(int i = 0; i < result.size(); i++){
+                    result.get(i).setMatkhau(null);
+                }
                 response.Data = result;
             } catch (Exception ex) {
                 response.Errors.add("Lỗi hệ thống.");
