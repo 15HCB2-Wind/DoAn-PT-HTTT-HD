3e8a6ab52c399aa3f34ad8b5ae32732ba8b744e7
tadtjolly <tuananh281293@gmail.com>
2017-05-25 19:01:11 +0700

    bổ sung config

---
+++ Wind_DrinkSmile_MiddleWare/DomainData/ChamSoc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainData
+{
+    public class ChamSoc
+    {
+        public string MaChamSoc { get; set; }
+
+        public float LuongSuaVat { get; set; }
+
+        public string TinhTrangCongViec { get; set; }
+
+        public bool DaDonVeSinh { get; set; }
+
+        public bool DaChoAn { get; set; }
+
+        public bool DaVatSua { get; set; }
+
+        public DateTime NgayGhiNhan { get; set; }
+
+        //khóa ngoại
+        public string MaPhanCong { get; set; }
+        public string MaBo { get; set; }
+    }
+}
