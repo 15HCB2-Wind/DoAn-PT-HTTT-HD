75a0fe37e2c23d81deaf863785f514acda7ffc91
4azuo <hiroshi.kaze1994@gmail.com>
2017-06-09 17:31:22 +0700

    update
    - remove service-report (no save report)

--- FrontEnd_DrinkSmile/FrontEnd_DrinkSmile/Scripts/config_hosts.js
+++ FrontEnd_DrinkSmile/FrontEnd_DrinkSmile/Scripts/config_hosts.js
@@ -3,15 +3,13 @@
         "login": "http://localhost:30000/",
         "management": "http://localhost:8080/Service_Management/webresources/",
         "import_export": "http://localhost:8080/Service_ImportExport/webresources/",
-        "animal_care": "http://localhost:28289/",
-        "report": ""
+        "animal_care": "http://localhost:28289/"
     },
     "hn": {
         "login": "",
         "management": "",
         "import_export": "",
-        "animal_care": "",
-        "report": ""
+        "animal_care": ""
     }
 }
 
